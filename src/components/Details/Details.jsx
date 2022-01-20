902c4c8f739e8e934c3f83fbb111231acaeebc90
Davia Deacon <ddeacon@dcisolutions.com>
2022-01-19 20:47:06 -0500

    - Added styles for App.js - Added title prop for Details component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import {Card, CardHeader, CardContent, Typography} from '@material-ui/core';
-import {Doughnut} from 'react-chartjs-2';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
 
 import useStyles from './styles';
 
-const Details = () => {
+const Details = ({ title }) => {
     const classes = useStyles();
-  return (
-      <Card className={classes.income}>
-          <CardHeader title="Income"/>
-          <CardContent>
-              <Typography variant='h5'>$50</Typography>
-              {/* <Doughnut data=""/> */}
-          </CardContent>
-      </Card>
-  );
+    return (
+        <Card className={title === 'Income' ? classes.income : classes.expense}>
+            <CardHeader title={title} />
+            <CardContent>
+                <Typography variant='h5'>$50</Typography>
+                {/* <Doughnut data=""/> */}
+            </CardContent>
+        </Card>
+    );
 };
 
 export default Details;
